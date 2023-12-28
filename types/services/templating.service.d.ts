217ea10ac5a0352d2864cbc886f01946acecf5b6
Juan Camejo Alarcón <juancamejo93@gmail.com>
2023-12-28 17:56:35 +0100

    remove types

--- types/services/templating.service.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const makeTemplate: (interviewData: any, template: string, options?: {
-    cleanHtml: boolean;
-}) => string;
-export declare const getCleanHtml: (text: string) => string;
--- types/services/utils.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { interviewParams, GenericQuestion } from "../types/General";
-export declare const isTest: () => boolean;
-export declare const generateRandomId: () => string;
-export declare const isCamelCase: (str: string) => boolean;
-export declare const isSnakeCase: (str: string) => boolean;
-export declare const validateParams: (params: interviewParams) => boolean;
-export declare const getValueBetweenRanges: (value: number, min: number, max: number) => number;
-export declare const validateSetValue: (value: string | number | string[], question: GenericQuestion) => void;
